225d8ebf94d6a986dc809744d9411280e8ea02a6
Nathan-Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-07-29 14:38:13 -0700

    Add core functionality for GQL operation name

--- spylib/constants.py
+++ spylib/constants.py
@@ -3,3 +3,7 @@ API_VERSION: str = '2021-04'
 RATE = 4
 MAX_TOKENS = 80
 THROTTLED_ERROR_MESSAGE = 'Throttled'
+OPERATION_NAME_REQUIRED_ERROR_MESSAGE = 'An operation name is required'
+WRONG_OPERATION_NAME_ERROR_MESSAGE = (
+    'No operation named "{}"'  # Required as they don't stay consistant
+)
