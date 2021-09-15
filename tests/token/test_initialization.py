88f92d506f5e5c61942fb7b1c65724e3062b137e
Nathan-Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-09-15 12:41:23 -0700

    Removed `new` calls for OAuth

--- tests/token/test_initialization.py
+++ tests/token/test_initialization.py
@@ -1,5 +1,3 @@
-from unittest.mock import AsyncMock
-
 import pytest
 
 from spylib import Token
@@ -46,47 +44,6 @@ async def test_online_token(
         expires_in=online_token_data.expires_in,
         associated_user_scope=online_token_data.associated_user_scope.split(','),
         associated_user=online_token_data.associated_user,
-    )
-
-    assert online_token.access_token == online_token_data.access_token
-    assert not online_token.access_token_invalid
-    assert online_token.scope == online_token_data.scope.split(',')
-    assert online_token.associated_user == online_token_data.associated_user
-    assert online_token.associated_user_scope == online_token_data.associated_user_scope.split(',')
-    assert online_token.api_url == (
-        f'https://{app_information.store_name}.myshopify.com/admin/'
-        + f'api/{app_information.api_version}'
-    )
-    assert (
-        online_token.oauth_url
-        == f'https://{app_information.store_name}.myshopify.com/admin/oauth/access_token'
-    )
-
-
-@pytest.mark.asyncio
-async def test_new_online_token(
-    mocker,
-    OnlineToken,
-    online_token_data: OnlineTokenResponse,
-    app_information: AppInformation,
-    MockHTTPResponse,
-):
-    """
-    This tests the ability to generate a new token from the endpoint using the
-    client_id, client_secret, and code.
-    """
-    # Generating the object callback with the token information
-    shopify_request_mock = mocker.patch('httpx.AsyncClient.request', new_callable=AsyncMock)
-    shopify_request_mock.side_effect = [
-        MockHTTPResponse(status_code=200, jsondata=online_token_data),
-    ]
-
-    # Create a new token
-    online_token = await OnlineToken.new(
-        app_information.store_name,
-        app_information.client_id,
-        app_information.client_secret,
-        app_information.code,
     )
 
     assert online_token.access_token == online_token_data.access_token
@@ -170,46 +127,6 @@ async def test_offline_token(
         store_name=app_information.store_name,
         access_token=offline_token_data.access_token,
         scope=offline_token_data.scope.split(','),
-    )
-
-    assert offline_token.access_token == offline_token_data.access_token
-    assert offline_token.scope == offline_token_data.scope.split(',')
-    assert offline_token.store_name == app_information.store_name
-    assert not offline_token.access_token_invalid
-    assert offline_token.api_url == (
-        f'https://{app_information.store_name}.myshopify.com/admin/'
-        + f'api/{app_information.api_version}'
-    )
-    assert (
-        offline_token.oauth_url
-        == f'https://{app_information.store_name}.myshopify.com/admin/oauth/access_token'
-    )
-
-
-@pytest.mark.asyncio
-async def test_new_offline_token(
-    mocker,
-    OfflineToken,
-    offline_token_data: OfflineTokenResponse,
-    app_information: AppInformation,
-    MockHTTPResponse,
-):
-    """
-    This tests the ability to generate a new token from the endpoint using the
-    client_id, client_secret, and code.
-    """
-    # Generating the object callback with the token information
-    shopify_request_mock = mocker.patch('httpx.AsyncClient.request', new_callable=AsyncMock)
-    shopify_request_mock.side_effect = [
-        MockHTTPResponse(status_code=200, jsondata=offline_token_data),
-    ]
-
-    # Create a new token
-    offline_token = await OfflineToken.new(
-        app_information.store_name,
-        app_information.client_id,
-        app_information.client_secret,
-        app_information.code,
     )
 
     assert offline_token.access_token == offline_token_data.access_token
