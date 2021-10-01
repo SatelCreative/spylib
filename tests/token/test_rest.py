04af8263b83fb0ea1231bf4ae7666a8815feddc0
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2021-10-01 09:41:37 -0700

    Feature/token refactor (#23)
    * Basic Outline of Token Functionality
    
    * Small fix for db
    
    * Base token tests passing
    
    * Adapted existing tests to new token class
    
    * Splitting on online and offline tokens
    
    * Added changed based on feedback
    
    * Updated tests to match requested format
    
    * Fixed linting and typing errors
    
    * Fix issue with typing
    
    * Removed `new` calls for OAuth
    
    * Renamed save_token and load_token to save and load
    
    * Fixed user_scope to be subset in test
    
    * Fixed token to use instance var instead of class var
    
    * Changed Online Token to only take id instead of full user
    
    * Fixed MockHTTPResponse to fit review
    
    * Basic README update
    
    * Remove tests based on feedback
    
    * Changed to class from fixture main class POC
    
    * Removed unessisary fixtures from classes
    
    * Fixed Linting and Formatting
    
    * Fixed typing errors including wrong exception base type
    
    * Undo change to exception type
    
    * Fixed order of Token definitions in docs
    
    * Small change to class docs
    
    * Updated test information to have better name
    
    * Changed tests
    
    * Removed user_scope from online token

--- tests/token/test_rest.py
+++ tests/token/test_rest.py
@@ -3,15 +3,14 @@ from unittest.mock import AsyncMock
 
 import pytest
 
-from spylib import Store
 from spylib.exceptions import ShopifyCallInvalidError
 
-from ..shared import MockHTTPResponse
+from ..token_classes import MockHTTPResponse, OfflineToken, test_information
 
 
 @pytest.mark.asyncio
 async def test_store_rest_happypath(mocker):
-    store = Store(store_id='TEST', name='test-store', access_token='Te5tM3')
+    token = await OfflineToken.load(store_name=test_information.store_name)
 
     shopify_request_mock = mocker.patch(
         'httpx.AsyncClient.request',
@@ -19,7 +18,7 @@ async def test_store_rest_happypath(mocker):
         return_value=MockHTTPResponse(status_code=200, jsondata={'success': True}),
     )
 
-    jsondata = await store.shoprequest(
+    jsondata = await token.execute_rest(
         goodstatus=200, debug='Test failed', endpoint='/test.json', method='get'
     )
 
@@ -28,12 +27,12 @@ async def test_store_rest_happypath(mocker):
     assert jsondata == {'success': True}
 
     # 80 from assuming Shopify plus then 1 used just now.
-    assert store.tokens == 79
+    assert token.rest_bucket == 79
 
 
 @pytest.mark.asyncio
 async def test_store_rest_badrequest(mocker):
-    store = Store(store_id='TEST', name='test-store', access_token='Te5tM3')
+    token = await OfflineToken.load(store_name=test_information.store_name)
 
     shopify_request_mock = mocker.patch(
         'httpx.AsyncClient.request',
@@ -44,7 +43,7 @@ async def test_store_rest_badrequest(mocker):
     )
 
     with pytest.raises(ShopifyCallInvalidError):
-        await store.shoprequest(
+        await token.execute_rest(
             goodstatus=201,
             debug='Test failed',
             endpoint='/products.json',
@@ -66,24 +65,29 @@ params = [
 
 @pytest.mark.parametrize('init_tokens, time_passed, expected_tokens', params)
 @pytest.mark.asyncio
-async def test_store_rest_ratetokens(init_tokens, time_passed, expected_tokens, mocker):
-    store = Store(store_id='TEST', name='test-store', access_token='Te5tM3')
+async def test_store_rest_ratetokens(
+    init_tokens,
+    time_passed,
+    expected_tokens,
+    mocker,
+):
+    token = await OfflineToken.load(store_name=test_information.store_name)
 
     # Simulate that there is only 2 calls available before hitting the rate limit.
     # If we set this to zero, then the code will wait 1 sec which is not great to keep the tests
     # fast
-    store.tokens = init_tokens
-    store.updated_at = monotonic() - time_passed  # A looooong time ago
+    token.rest_bucket = init_tokens
+    token.updated_at = monotonic() - time_passed  # A looooong time ago
 
     shopify_request_mock = mocker.patch(
         'httpx.AsyncClient.request',
         new_callable=AsyncMock,
         return_value=MockHTTPResponse(status_code=200, jsondata={'success': True}),
     )
-    await store.shoprequest(
+    await token.execute_rest(
         goodstatus=200, debug='Test failed', endpoint='/test.json', method='get'
     )
 
     shopify_request_mock.assert_called_once()
 
-    assert store.tokens == expected_tokens
+    assert token.rest_bucket == expected_tokens
