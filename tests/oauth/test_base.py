2c0c8e8ab5bd0a9e8afba4e88791a4d6f49bc6cc
lishanl <lishan@satel.ca>
2022-03-19 02:37:55 -0700

    separate the without and with fastapi oauth test

--- tests/oauth/test_base.py
+++ tests/oauth/test_base.py
@@ -1,15 +1,11 @@
+from importlib import util
+from sys import modules
 from typing import List
 from unittest.mock import AsyncMock
 from urllib.parse import ParseResult, parse_qs, urlencode, urlparse
 
 import pytest
 from box import Box  # type: ignore
-
-try:
-    from fastapi import FastAPI  # type: ignore
-    from fastapi.testclient import TestClient  # type: ignore
-except ImportError:
-    pass
 from pydantic.dataclasses import dataclass
 from requests import Response  # type: ignore
 
@@ -65,106 +61,116 @@ class MockHTTPResponse:
 
 
 @pytest.mark.asyncio
-async def test_oauth(mocker):
-
+async def test_oauth_without_fastapi():
     with pytest.raises(FastAPIImportError):
-        from spylib.oauth import OfflineToken, OnlineToken, init_oauth_router
+        import spylib.oauth  # noqa
 
-        app = FastAPI()
 
-        oauth_router = init_oauth_router(**TEST_DATA)
+@pytest.mark.asyncio
+async def test_oauth_with_fastapi(mocker):
+    if 'fastapi' not in modules and util.find_spec('fastapi') is None:
+        return
 
-        app.include_router(oauth_router)
-        client = TestClient(app)
+    from fastapi import FastAPI  # type: ignore
+    from fastapi.testclient import TestClient  # type: ignore
 
-        # --------- Test the initialization endpoint -----------
+    from spylib.oauth import OfflineToken, OnlineToken, init_oauth_router
 
-        # Missing shop argument
-        response = client.get('/shopify/auth')
-        assert response.status_code == 422
-        assert response.json() == {
-            'detail': [
-                {'loc': ['query', 'shop'], 'msg': 'field required', 'type': 'value_error.missing'}
-            ],
-        }
+    app = FastAPI()
 
-        # Happy path
-        response = client.get('/shopify/auth', params=dict(shop=TEST_STORE), allow_redirects=False)
-        query = check_oauth_redirect_url(
-            response=response,
-            client=client,
-            path='/admin/oauth/authorize',
-            scope=TEST_DATA.app_scopes,
-        )
-        state = check_oauth_redirect_query(query=query, scope=TEST_DATA.app_scopes)
+    oauth_router = init_oauth_router(**TEST_DATA)
 
-        # Callback calls to get tokens
-        shopify_request_mock = mocker.patch('httpx.AsyncClient.request', new_callable=AsyncMock)
-        shopify_request_mock.side_effect = [
-            MockHTTPResponse(status_code=200, jsondata=OFFLINETOKEN_DATA),
-            MockHTTPResponse(status_code=200, jsondata=ONLINETOKEN_DATA),
-        ]
-        # --------- Test the callback endpoint for installation -----------
-        query_str = urlencode(
-            dict(shop=TEST_STORE, state=state, timestamp=now_epoch(), code='INSTALLCODE')
-        )
-        hmac_arg = hmac.calculate_from_message(secret=SHOPIFY_SECRET_KEY, message=query_str)
-        query_str += '&hmac=' + hmac_arg
+    app.include_router(oauth_router)
+    client = TestClient(app)
 
-        response = client.get('/callback', params=query_str, allow_redirects=False)
-        query = check_oauth_redirect_url(
-            response=response,
-            client=client,
-            path='/admin/oauth/authorize',
-            scope=TEST_DATA.user_scopes,
-        )
-        state = check_oauth_redirect_query(
-            query=query,
-            scope=TEST_DATA.user_scopes,
-            query_extra={'grant_options[]': ['per-user']},
-        )
+    # --------- Test the initialization endpoint -----------
 
-        assert await shopify_request_mock.called_with(
-            method='post',
-            url=f'https://{TEST_STORE}/admin/oauth/access_token',
-            json={
-                'client_id': SHOPIFY_API_KEY,
-                'client_secret': SHOPIFY_SECRET_KEY,
-                'code': 'INSTALLCODE',
-            },
-        )
+    # Missing shop argument
+    response = client.get('/shopify/auth')
+    assert response.status_code == 422
+    assert response.json() == {
+        'detail': [
+            {'loc': ['query', 'shop'], 'msg': 'field required', 'type': 'value_error.missing'}
+        ],
+    }
 
-        TEST_DATA.post_install.assert_called_once()
-        TEST_DATA.post_install.assert_called_with('test', OfflineToken(**OFFLINETOKEN_DATA))
+    # Happy path
+    response = client.get('/shopify/auth', params=dict(shop=TEST_STORE), allow_redirects=False)
+    query = check_oauth_redirect_url(
+        response=response,
+        client=client,
+        path='/admin/oauth/authorize',
+        scope=TEST_DATA.app_scopes,
+    )
+    state = check_oauth_redirect_query(query=query, scope=TEST_DATA.app_scopes)
 
-        # --------- Test the callback endpoint for login -----------
-        query_str = urlencode(
-            dict(shop=TEST_STORE, state=state, timestamp=now_epoch(), code='LOGINCODE'),
-            safe='=,&/[]:',
-        )
-        hmac_arg = hmac.calculate_from_message(secret=SHOPIFY_SECRET_KEY, message=query_str)
-        query_str += '&hmac=' + hmac_arg
+    # Callback calls to get tokens
+    shopify_request_mock = mocker.patch('httpx.AsyncClient.request', new_callable=AsyncMock)
+    shopify_request_mock.side_effect = [
+        MockHTTPResponse(status_code=200, jsondata=OFFLINETOKEN_DATA),
+        MockHTTPResponse(status_code=200, jsondata=ONLINETOKEN_DATA),
+    ]
+    # --------- Test the callback endpoint for installation -----------
+    query_str = urlencode(
+        dict(shop=TEST_STORE, state=state, timestamp=now_epoch(), code='INSTALLCODE')
+    )
+    hmac_arg = hmac.calculate_from_message(secret=SHOPIFY_SECRET_KEY, message=query_str)
+    query_str += '&hmac=' + hmac_arg
 
-        response = client.get('/callback', params=query_str, allow_redirects=False)
-        state = check_oauth_redirect_url(
-            response=response,
-            client=client,
-            path=f'/admin/apps/{HANDLE}',
-            scope=TEST_DATA.user_scopes,
-        )
+    response = client.get('/callback', params=query_str, allow_redirects=False)
+    query = check_oauth_redirect_url(
+        response=response,
+        client=client,
+        path='/admin/oauth/authorize',
+        scope=TEST_DATA.user_scopes,
+    )
+    state = check_oauth_redirect_query(
+        query=query,
+        scope=TEST_DATA.user_scopes,
+        query_extra={'grant_options[]': ['per-user']},
+    )
 
-        assert await shopify_request_mock.called_with(
-            method='post',
-            url=f'https://{TEST_STORE}/admin/oauth/access_token',
-            json={
-                'client_id': SHOPIFY_API_KEY,
-                'client_secret': SHOPIFY_SECRET_KEY,
-                'code': 'LOGINCODE',
-            },
-        )
+    assert await shopify_request_mock.called_with(
+        method='post',
+        url=f'https://{TEST_STORE}/admin/oauth/access_token',
+        json={
+            'client_id': SHOPIFY_API_KEY,
+            'client_secret': SHOPIFY_SECRET_KEY,
+            'code': 'INSTALLCODE',
+        },
+    )
 
-        TEST_DATA.post_login.assert_called_once()
-        TEST_DATA.post_login.assert_called_with('test', OnlineToken(**ONLINETOKEN_DATA))
+    TEST_DATA.post_install.assert_called_once()
+    TEST_DATA.post_install.assert_called_with('test', OfflineToken(**OFFLINETOKEN_DATA))
+
+    # --------- Test the callback endpoint for login -----------
+    query_str = urlencode(
+        dict(shop=TEST_STORE, state=state, timestamp=now_epoch(), code='LOGINCODE'),
+        safe='=,&/[]:',
+    )
+    hmac_arg = hmac.calculate_from_message(secret=SHOPIFY_SECRET_KEY, message=query_str)
+    query_str += '&hmac=' + hmac_arg
+
+    response = client.get('/callback', params=query_str, allow_redirects=False)
+    state = check_oauth_redirect_url(
+        response=response,
+        client=client,
+        path=f'/admin/apps/{HANDLE}',
+        scope=TEST_DATA.user_scopes,
+    )
+
+    assert await shopify_request_mock.called_with(
+        method='post',
+        url=f'https://{TEST_STORE}/admin/oauth/access_token',
+        json={
+            'client_id': SHOPIFY_API_KEY,
+            'client_secret': SHOPIFY_SECRET_KEY,
+            'code': 'LOGINCODE',
+        },
+    )
+
+    TEST_DATA.post_login.assert_called_once()
+    TEST_DATA.post_login.assert_called_with('test', OnlineToken(**ONLINETOKEN_DATA))
 
 
 def check_oauth_redirect_url(response: Response, client, path: str, scope: List[str]) -> str:
