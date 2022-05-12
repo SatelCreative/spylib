865b3e7ec8a8fdf73cfb8824ee68d76c98e1c9c3
Frank Chung <ponty33@gmail.com>
2022-05-12 13:46:49 -0700

    update md

--- docs/multipass.md
+++ docs/multipass.md
@@ -3,20 +3,17 @@
 This helper class generates token or URL that's needed for Shopify Multipass login.
 
 ```python
-from spylib import Multipass
+from spylib import multipass
 
 
 # Customer email is required to generate token or URL
-customer_data = {"email": "customer@email.com"}
-
-# Initiate the Multipass class
-multipass = Multipass("MULTIPASS_SECRET")
+contact_data = {"email": "customer@email.com"}
 
 # Generate URL
-url = multipass.generate_url(customer_data, 'https://example.myshopify.com')
+url = multipass.generate_url("MULTIPASS_SECRET", contact_data, 'https://example.myshopify.com')
 # https://example.myshopify.com/account/login/multipass/{MultipassToken}
 
-# If for some reason you need it, you can also generate separately the token used in the URL:
-token = multipass.generate_token(customer_data)
+# If for some reason you need the token, you can also generate the token used in the URL separately:
+token = multipass.generate_token("MULTIPASS_SECRET", contact_data)
 
 ```
--- spylib/multipass.py
+++ spylib/multipass.py
@@ -8,18 +8,18 @@ from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
 
 
-def generate_token(secret: str, contact_raw_data: Dict[str, Any]) -> bytes:
+def generate_token(secret: str, contact_data: Dict[str, Any]) -> bytes:
     key = SHA256.new(secret.encode('utf-8')).digest()
     encryption_key = key[0:16]
     signature_key = key[16:32]
 
-    contact_raw_data['created_at'] = datetime.datetime.utcnow().isoformat()
-    cypher_text = _encrypt(encryption_key, json.dumps(contact_raw_data))
+    contact_data['created_at'] = datetime.datetime.utcnow().isoformat()
+    cypher_text = _encrypt(encryption_key, json.dumps(contact_data))
     return urlsafe_b64encode(cypher_text + _sign(signature_key, cypher_text))
 
 
-def generate_url(secret: str, contact_raw_data: Dict[str, Any], url) -> str:
-    token = generate_token(secret, contact_raw_data).decode('utf-8')
+def generate_url(secret: str, contact_data: Dict[str, Any], url) -> str:
+    token = generate_token(secret, contact_data).decode('utf-8')
     return f'{url}/account/login/multipass/{token}'
 
 
