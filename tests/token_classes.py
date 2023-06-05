468dbce1d1febbc701411088bf05b24f5f80b486
Frank Chung <ponty33@gmail.com>
2023-06-05 11:37:11 -0700

    :sparkles: Remove default API version and make it manual input (#183)
    * remove default api version
    
    * make API version a manual input
    
    * add test and revert
    
    * same place
    
    * change
    
    * edit

--- tests/token_classes.py
+++ tests/token_classes.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import ClassVar, Optional
+
 from pydantic.class_validators import validator
 from pydantic.main import BaseModel
 
@@ -47,7 +49,7 @@ class TestInformation(BaseModel):
     Information about the store for tests.
     """
 
-    api_version: str = '2022-01'
+    api_version: str = '2023-04'
     store_name: str = 'Test-Store'
     client_id: int = 1
     client_secret: int = 2
@@ -92,6 +94,21 @@ class PrivateToken(PrivateTokenABC):
     @classmethod
     async def load(cls, store_name: str) -> PrivateToken:
         return PrivateToken(
+            access_token=offline_token_data.access_token,
+            scope=offline_token_data.scope,
+            store_name=test_information.store_name,
+        )
+
+
+class VersionOfflineToken(OfflineTokenABC):
+    api_version: ClassVar[Optional[str]] = test_information.api_version
+
+    async def save(self):
+        pass
+
+    @classmethod
+    async def load(cls, store_name: str) -> VersionOfflineToken:
+        return VersionOfflineToken(
             access_token=offline_token_data.access_token,
             scope=offline_token_data.scope,
             store_name=test_information.store_name,
